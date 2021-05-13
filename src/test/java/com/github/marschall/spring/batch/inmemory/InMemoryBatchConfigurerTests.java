ca77e8097ffb65f8f10aa992eb049e4ade8cc3b9
Philippe Marschall <philippe.marschall@gmail.com>
2021-05-13 20:18:32 +0200

    more test configuration

--- src/test/java/com/github/marschall/spring/batch/inmemory/InMemoryBatchConfigurerTests.java
+++ src/test/java/com/github/marschall/spring/batch/inmemory/InMemoryBatchConfigurerTests.java
@@ -5,13 +5,26 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.SimpleBatchConfiguration;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
 
-//@SpringBatchTest
+import com.github.marschall.spring.batch.inmemory.InMemoryBatchConfigurerTests.TestConfiguration;
+
+@SpringBatchTest
+@ContextConfiguration(classes = TestConfiguration.class)
 class InMemoryBatchConfigurerTests {
+
+  @Autowired
+  private JobLauncherTestUtils jobLauncherTestUtils;
+
+  @Autowired
+  private JobRepositoryTestUtils jobRepositoryTestUtils;
 
   @Test
   void test() {
