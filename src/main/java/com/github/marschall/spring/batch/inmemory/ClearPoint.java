c1b9022243edb9a4b55cbb320027adfa5fcf859f
Philippe Marschall <philippe.marschall@gmail.com>
2021-08-20 11:18:33 +0200

    better javadoc

--- src/main/java/com/github/marschall/spring/batch/inmemory/ClearPoint.java
+++ src/main/java/com/github/marschall/spring/batch/inmemory/ClearPoint.java
@@ -7,8 +7,14 @@ package com.github.marschall.spring.batch.inmemory;
  */
 public enum ClearPoint {
 
+  /**
+   * {@link InMemoryJobStorage} should be cleared after every test execution.
+   */
   AFTER_TEST,
 
+  /**
+   * {@link InMemoryJobStorage} should be cleared after all tests in all tests in a class ran.
+   */
   AFTER_CLASS;
 
 }
